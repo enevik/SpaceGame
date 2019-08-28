b8a45d4cace55ade3465160a0b10677c4706cce1
Unknown <enevik@hotmail.com>
2019-08-28 17:10:47 +0200

    homepage aangemaakt
    ik heb de homepage aangemaakt. GEEN KLEUREN. erg kaal

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SpaceGame</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <div class="headerflex">
+        <div>
+            <!-- placeholder for the logo <-->
+            <h1>Logo</h1>
+        </div>
+        <div>
+            <div class="navbar">
+                <a href="#home">Home</a>
+                <a href="#shop">shop</a>
+                <a href="#tasks">tasks</a>
+                <a href="#station">station</a>
+            </div>
+        </div>
+        <div class="cash">
+            <p>Cash</p>
+        </div>
+    </div>
+</header>
+<main>
+    <div class="container">
+        <div class="maingrid">
+            <div class="personalinformation">
+                <h2>Persoonlijke informatie</h2>
+                <div>
+                    <p>naam</p>
+                    <p>schepen</p>
+                    <p>cash</p>
+                </div>
+            </div>
+            <div class="gif">
+                <h2> Hier komt een Gif</h2>
+            </div>
+            <div class="tasks">
+                <h2>Tasks</h2>
+                <div>
+                    <p>mars</p>
+                    <p>jupiter</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<footer>
+    <div class="container">
+
+    </div>
+</footer>
+</body>
+</html>
