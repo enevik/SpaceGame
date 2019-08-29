44b85926d3dfd16e925ede5670cbe387ec833fd9
Dani <d.kools1@outlook.com>
2019-08-29 12:40:39 +0200

    dingen veranderd op tasks pagina

--- tasks.php
+++ tasks.php
@@ -9,7 +9,30 @@
 require 'header.php';
 ?>
 <div class="container">
-    <h1>hey</h1>
+    <h1>Tasks</h1>
+    <div class="planet">
+        <div class="title">
+            <h2>Planeet : Mars</h2>
+        </div>
+        <div class="image">
+            <img src="images/mars-image.jpg" alt="">
+        </div>
+        <div class="info">
+            <h3>Informatie over mars</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum error labore, magnam maiores modi officiis quis similique. Animi at cumque eos, ex iste quod reiciendis velit vitae voluptas voluptatibus? Repellendus.</p>
+        </div>
+        <div class="task">
+            <div class="info-task">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aliquid, asperiores beatae cupiditate delectus eligendi iste, libero maxime, minus modi necessitatibus omnis quas quasi suscipit? Magni non ratione voluptatibus?</p>
+            </div>
+            <div class="commodities">
+                <p>1.200</p>
+            </div>
+            <div class="button">
+                <button>Go!</button>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php
