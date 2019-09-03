e59faeed414d6d0789fdf9e62d9561a79f23e842
Esma Yilmaz <Esma_yilmaz7@hotmail.com>
2019-09-03 13:07:58 +0200

    upgradestation

---
+++ upgradestation.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+    <header>
+        <div class="headermars">
+            <div class="headertwomars">
+	            <h2>Upgrade station</h2>
+                <h3 class="marsspace">Mars</h3>
+            </div>
+            <div class="headermarsthree">
+                <img class="onespace" src="img/49949736-vliegende-schotel-ruimteschip-geïsoleerd-op-een-witte-achtergrond-als-een-klassiek-ufo-buitenaards-hov.jpg" alt="">
+                <div>
+                    <ul class="marslist">
+                        <li>Motor<button class="oneupdatebutton">Update</button></li>
+                        <li>Brandstof<button class="twoupdatebutton">Update</button></li>
+                        <li>Vleugels<button class="threeupdatebutton">Update</button></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="headermarsfour">
+                <div class="headertwomarsfour">
+                    <h3 class="jupiterspace">Jupiter</h3>
+                </div>
+                <div class="headermarsthreefour">
+                    <img src="img/49949736-vliegende-schotel-ruimteschip-geïsoleerd-op-een-witte-achtergrond-als-een-klassiek-ufo-buitenaards-hov.jpg" alt="">
+                    <div>
+                        <ul class="jupiterlist">
+                            <li>Motor<button class="oneupdatebutton">Update</button></li>
+                            <li>Brandstof<button class="twoupdatebutton">Update</button></li>
+                            <li>Vleugels<button class="threeupdatebutton">Update</button></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="headermarsfourtwo">
+                    <div class="headertwomarsfourtwo">
+                        <h3 class="venusspace">Venus</h3>
+                    </div>
+                    <div class="headermarsthreefourtwo">
+                        <img src="img/49949736-vliegende-schotel-ruimteschip-geïsoleerd-op-een-witte-achtergrond-als-een-klassiek-ufo-buitenaards-hov.jpg" alt="">
+                        <div>
+                            <ul class="venuslist">
+                                <li>Motor<button class="oneupdatebutton">Update</button></li>
+                                <li>Brandstof<button class="twoupdatebutton">Update</button></li>
+                                <li>Vleugels<button class="threeupdatebutton">Update</button></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="headermarsfivetwo">
+                        <div class="headertwomarsfivetwo">
+                            <h3 class="mercuriusspace">Mercurius</h3>
+                        </div>
+                        <div class="headermarsthreefivetwo">
+                            <img src="img/49949736-vliegende-schotel-ruimteschip-geïsoleerd-op-een-witte-achtergrond-als-een-klassiek-ufo-buitenaards-hov.jpg" alt="">
+                            <div>
+                                <ul class="mercuriuslist">
+                                    <li>Motor<button class="oneupdatebutton">Update</button></li>
+                                    <li>Brandstof<button class="twoupdatebutton">Update</button></li>
+                                    <li>Vleugels<button class="threeupdatebutton">Update</button></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="headermarsfourthree">
+                            <div class="headertwomarsfourthree">
+                                <h3 class="saturnusspace">Saturnus</h3>
+                            </div>
+                            <div class="headermarsthreefourthree">
+                                <img src="img/49949736-vliegende-schotel-ruimteschip-geïsoleerd-op-een-witte-achtergrond-als-een-klassiek-ufo-buitenaards-hov.jpg" alt="">
+                                <div>
+                                    <ul class="saturnuslist">
+                                        <li>Motor<button class="oneupdatebutton">Update</button></li>
+                                        <li>Brandstof<button class="twoupdatebutton">Update</button></li>
+                                        <li>Vleugels<button class="threeupdatebutton">Update</button></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="headerfourmarsfourone">
+                                <div class="headerfourmarsfourtwo">
+                                    <h3 class="uranusspace">Uranus</h3>
+                                </div>
+                                <div class="headerfourmarsthreefour">
+                                    <img src="img/49949736-vliegende-schotel-ruimteschip-geïsoleerd-op-een-witte-achtergrond-als-een-klassiek-ufo-buitenaards-hov.jpg" alt="">
+                                    <div>
+                                        <ul class="uranuslist">
+                                            <li>Motor<button class="oneupdatebutton">Update</button></li>
+                                            <li>Brandstof<button class="twoupdatebutton">Update</button></li>
+                                            <li>Vleugels<button class="threeupdatebutton">Update</button></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="headermarsfivethree">
+                                    <div class="headertwomarsfivethree">
+                                        <h3 class="neptunusspace">Neptunus</h3>
+                                    </div>
+                                    <div class="headermarsthreefivethree">
+                                        <img src="img/49949736-vliegende-schotel-ruimteschip-geïsoleerd-op-een-witte-achtergrond-als-een-klassiek-ufo-buitenaards-hov.jpg" alt="">
+                                        <div>
+                                            <ul class="neptunuslist">
+                                                <li>Motor<button class="oneupdatebutton">Update</button></li>
+                                                <li>Brandstof<button class="twoupdatebutton">Update</button></li>
+                                                <li>Vleugels<button class="threeupdatebutton">Update</button></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+</body>
+
+</html>
